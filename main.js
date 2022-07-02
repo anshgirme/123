99104e2002ce0d71ce738b51133130285beb42a8
anshgirme <89573959+anshgirme@users.noreply.github.com>
2022-07-02 18:57:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+difference=0;
+rightWristX=0;
+leftWristX=0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -13,12 +17,23 @@ function modelLoaded() {
     console.log('PoseNet Is Initialized!');
 }
 
-function draw() {
-    background('#969A97');
-}
-
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX + " difference = " + difference);
     }
+}
+
+function draw() {
+    background('#969A97');
+
+    document.getElementById("font_size").innerHTML = "font size of the TEXT will be =" + difference + "px";
+    textSize(difference);
+   // fill('cyan');
+    stroke('red');
+    text('Ansh', 50, 400)
 }
